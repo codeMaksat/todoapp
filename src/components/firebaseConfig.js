e4ffa7da608f8d408e54e3adb2844a87a329f084
codeMaksat <maksatylyas@gmail.com>
2022-01-26 11:17:23 +0500

    why

--- src/components/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyDIcQzFeHiiP595mgQlDAKxA5Icsqcw3Gs",
-    authDomain: "todoapp-a1bed.firebaseapp.com",
-    projectId: "todoapp-a1bed",
-    storageBucket: "todoapp-a1bed.appspot.com",
-    messagingSenderId: "191050388213",
-    appId: "1:191050388213:web:5f877673712a1e79cbabbc",
-    measurementId: "G-GMT2E9J94C"
-}
--- src/components/firebaseInit.js
+++
@@ -1,7 +0,0 @@
-import { initializeApp } from 'firebase/app'
-import 'firebase/firestore'
-import firebaseConfig from './firebaseConfig'
-
-const firebaseApp = initializeApp(firebaseConfig)
-
-export default firebaseApp.firestore()
